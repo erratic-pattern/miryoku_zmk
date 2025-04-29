3cef528980e42d989d7e45b259784faf5b450ff7
Adam Curtis <adam.curtis.dev@gmail.com>
2025-04-29 15:29:44 -0500

    adjust tapping term to 180 ms

--- miryoku/miryoku.h
+++ miryoku/miryoku.h
@@ -23,7 +23,7 @@
 #define U_NA &none // present but not available for use
 #define U_NU &none // available but not used
 
-#define U_TAPPING_TERM 200
+#define U_TAPPING_TERM 180
 #define U_QUICK_TAP_MS U_TAPPING_TERM
 
 #include "miryoku_clipboard.h"
